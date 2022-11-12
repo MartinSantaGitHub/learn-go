64b01fb2c92d36c6f9235253f28d0a46870c0c3e
martins <santasan27@gmail.com>
2022-11-11 21:25:27 -0300

    Test defer

--- excercise-01/main.go
+++ excercise-01/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type User struct {
+	Age int
+}
+
 func main() {
+	var u *User
+
+	defer func() {
+		fmt.Println("Crash!")
+	}()
+
+	u.Age = 10
+
+	fmt.Println(u)
 	fmt.Println("Hello World!")
 }
